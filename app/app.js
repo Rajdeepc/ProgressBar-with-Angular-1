cec08b5308db9f557eb8a81f21fe6a22b135edda
Rajdeepc <rajrock38@gmail.com>
2017-07-23 12:49:59 +0530

    first commit

--- app/app.js
+++ app/app.js
@@ -1,1 +1,39 @@
-var app = angular.module('progress',[]);
+var myApp = angular.module('progress', ['ui.bootstrap']);
+
+(function(app){
+"use strict";
+app.controller('progressController',function ($scope) {
+
+  $scope.dynamic = 10;
+  $scope.max = 230;
+
+  $scope.add10 = function(){
+    $scope.dynamic = $scope.dynamic + 10;
+  };
+   $scope.add25 = function(){
+    $scope.dynamic = $scope.dynamic + 25;
+  };
+   $scope.minus10 = function(){
+     if($scope.dynamic > 0 ){
+    $scope.dynamic = $scope.dynamic - 10;
+     }
+     else
+       {
+         $scope.dynamic = 0;
+       }
+  };
+   $scope.minus25 = function(){
+      if($scope.dynamic > 0 ){
+    $scope.dynamic = $scope.dynamic - 25;
+     }
+     else
+       {
+         $scope.dynamic = 0;
+       }
+   
+  };
+  $scope.reset = function(){
+    $scope.dynamic = 10;
+  };
+});
+})(myApp);
